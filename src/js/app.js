d43f768e8a9b85d583c87e06e048627888ee9dff
sanchyopas <saniagolovanev@gmail.com>
2025-05-13 17:55:59 +0700

    dev single page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,27 @@
 import * as functions from './modules/baseFunctions.js';
 
 functions.isWebp();
+
+
+const indicator = document.querySelector('#active-element');
+const truckNavElement = document.querySelectorAll('.truck__nav-item');
+
+function moveIndicator(el) {
+  const { offsetLeft, offsetWidth } = el;
+  indicator.style.left = offsetLeft + 'px';
+  indicator.style.width = offsetWidth + 'px';
+}
+
+truckNavElement?.forEach(el => {
+  el.addEventListener('click', () => {
+    truckNavElement.forEach((el) => {el.classList.remove('_active');});
+    el.classList.add('_active');
+    moveIndicator(el);
+  });
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  if(truckNavElement[0]) {
+    moveIndicator(truckNavElement[0]);
+  }
+})
