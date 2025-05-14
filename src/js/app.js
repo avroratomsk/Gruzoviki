48dcde5386a156dcfa27b5f3135733abd91a281c
sanchyopas <saniagolovanev@gmail.com>
2025-05-14 14:31:55 +0700

    dev page single

--- src/js/app.js
+++ src/js/app.js
@@ -6,22 +6,70 @@ functions.isWebp();
 const indicator = document.querySelector('#active-element');
 const truckNavElement = document.querySelectorAll('.truck__nav-item');
 
-function moveIndicator(el) {
+function moveIndicator(el, nameIndiator) {
   const { offsetLeft, offsetWidth } = el;
-  indicator.style.left = offsetLeft + 'px';
-  indicator.style.width = offsetWidth + 'px';
+  nameIndiator.style.left = offsetLeft + 'px';
+  nameIndiator.style.width = offsetWidth + 'px';
 }
 
 truckNavElement?.forEach(el => {
   el.addEventListener('click', () => {
     truckNavElement.forEach((el) => {el.classList.remove('_active');});
     el.classList.add('_active');
-    moveIndicator(el);
+    moveIndicator(el, indicator);
   });
+})
+
+
+
+const optionBtns = document.querySelectorAll('.wheelbase__option');
+const indicatorOption = document.querySelector('.wheelbase__indicator');
+
+optionBtns?.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    optionBtns.forEach((el) => {el.classList.remove('_active');});
+    btn.classList.add('_active');
+    moveIndicator(btn, indicatorOption);
+
+    let fields = [
+      "length", "width", "height", "weight", "lifting", "structure", "structure",
+      "structurewidth", "structureheight", "volume", "pallets", "capacity",
+    ]
+    const imageEl = document.getElementById("image");
+    const sourceEl = imageEl?.previousElementSibling?.tagName === "SOURCE"
+      ? imageEl.previousElementSibling
+      : imageEl?.parentElement?.querySelector("source");
+    fields.forEach((field) => {
+      const value = btn.dataset[field];
+      const el = document.getElementById(field);
+
+      if(el) {
+        if(field === "image"){
+          console.log(value)
+          el.src = value
+        }else {
+          el.innerText = value;
+        }
+      }
+    })
+
+    const imagePath = btn.dataset.image;
+    if (imageEl && sourceEl && imagePath) {
+      imageEl.src = imagePath;
+      sourceEl.srcset = imagePath;
+    }
+
+
+  })
 })
 
 window.addEventListener('DOMContentLoaded', () => {
   if(truckNavElement[0]) {
-    moveIndicator(truckNavElement[0]);
+    moveIndicator(truckNavElement[0], indicator);
   }
+
+  if(optionBtns[0]) {
+    moveIndicator(optionBtns[0], indicatorOption);
+  }
+
 })
